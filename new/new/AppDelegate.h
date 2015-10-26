5e245c02c03694a623d005a0aed72d57c36f45de
dn <dn@163.com>
2015-10-27 06:50:44 +0800

    aca

---
+++ new/new/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  new
+//
+//  Created by imac on 15/10/27.
+//  Copyright (c) 2015年 neusoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ new/new/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  new
+//
+//  Created by imac on 15/10/27.
+//  Copyright (c) 2015年 neusoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
